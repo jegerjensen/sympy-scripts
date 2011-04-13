b10d4256cf1dcb5cbf6a4716efdf5a29b9c2dde3
Øyvind Jensen <jensen.oyvind@gmail.com>
2011-04-13 11:58:43 +0200

    Moved specialized reduced matrix elements into a separate file

--- check_recouplings_AAp1.py
+++ check_recouplings_AAp1.py
@@ -18,6 +18,8 @@ from sympy import (
         )
 import sys
 
+from utilities.redmats import ReducedMatrixElement_left, ReducedMatrixElement_right
+
 pprint_use_unicode(False)
 def _report(expr):
 
@@ -43,51 +45,6 @@ def _report(expr):
     # preview(expr)
 
     print
-
-class ReducedMatrixElement_right(ReducedMatrixElement):
-    _definition = "right"
-
-    def _eval_reduction_factor(self):
-        left, op, right = self.args
-        c_ket, t_ket = right.as_coeff_tensor()
-        j1, m1 = t_ket.get_rank_proj()
-        c_op, t_op = op.as_coeff_tensor()
-        j2, m2 = t_op.get_rank_proj()
-        J, M = left._j, left._m
-        return c_op*c_ket*ClebschGordanCoefficient(j2, m2, j1, m1, J, M)
-    def _eval_inverse_reduction_factor(self):
-        left,op,right = self.args
-        c_ket, t_ket = right.as_coeff_tensor()
-        j1, m1 = t_ket.get_rank_proj()
-        c_op, t_op = op.as_coeff_tensor()
-        j2, m2 = t_op.get_rank_proj()
-        J, M = left._j, left._m
-        return ASigma(m1, m2)*ClebschGordanCoefficient(j2, m2, j1, m1, J, M)/c_op/c_ket
-
-class ReducedMatrixElement_left(ReducedMatrixElement):
-    _definition = "left"
-
-    def _eval_reduction_factor(self):
-        left, op, right = self.args
-        c_bra, t_bra = left.as_coeff_tensor()
-        j1, m1 = t_bra.get_rank_proj()
-        c_op, t_op = op.as_coeff_tensor()
-        j2, m2 = t_op.get_rank_proj()
-        J, M = right._j, right._m
-        return c_op*c_bra*ClebschGordanCoefficient(j1, m1, j2, m2, J, -M)*(-1)**(J-M)
-
-    def _eval_inverse_reduction_factor(self):
-        left,op,right = self.args
-        left, op, right = self.args
-        c_bra, t_bra = left.as_coeff_tensor()
-        j1, m1 = t_bra.get_rank_proj()
-        c_op, t_op = op.as_coeff_tensor()
-        j2, m2 = t_op.get_rank_proj()
-        J, M = right._j, right._m
-        return ASigma(m1, m2)*ClebschGordanCoefficient(j1, m1, j2, m2, J, -M)/c_op/c_bra/(-1)**(J-M)
-import sympy
-sympy.physics.braket.ReducedMatrixElement_left = ReducedMatrixElement_left
-sympy.physics.braket.ReducedMatrixElement_right = ReducedMatrixElement_right
 
 def make_spherical_sp_states(str1):
     states = []
