4a568646ad07605bcc65ce3ed20386be8d83821e
Øyvind Jensen <jensen.oyvind@gmail.com>
2011-04-28 11:59:18 +0200

    ccm_code should use indexify from utilities

--- ccm_code.py
+++ ccm_code.py
@@ -17,6 +17,7 @@ from sympy.utilities.codegen import codegen
 from sympy.core.symbol import Dummy
 
 from utilities.ccm import get_CC_operators
+from utilities.indexify import indexify
 
 pretty_dummies_dict={
         'above':'cdefgh',
@@ -123,66 +124,6 @@ def main():
     eqT2_idx = indexify(Eq(lhs, eqT2))
     print eqT2_idx
     codegen(('t2_diagrams', eqT2_idx), 'f95', 'code/sympy_ccm_T2', to_files=True, project=project_descr)
-
-
-def indexify(expr):
-    """converts an expression with AntiSymmetricTensor to Indexed and Idx
-
-    This is useful for generation of code for Coupled-Cluster
-    """
-
-    all_AT = expr.atoms(AntiSymmetricTensor)
-
-    total_orbs, below_orbs = symbols('total_orbs below_orbs', integer=True)
-
-    subsdict = {}       # for index substitutions
-    fixed_inds = set()  # indices for left hand side
-    for at in all_AT:
-
-        for index in at.upper.args + at.lower.args:
-            if not isinstance(index, Dummy):
-                fixed_inds.add(index)
-                label = Symbol(index.name, integer=True)
-            else:
-                label = Symbol("%s_%i" %(index.name, index.dummy_index),
-                        integer=True)
-
-            if index not in subsdict:
-                # determine index range
-                if index.assumptions0.get('above_fermi'):
-                    start = below_orbs
-                else:
-                    start = 0
-                if index.assumptions0.get('below_fermi'):
-                    stop = below_orbs -1
-                else:
-                    stop = total_orbs -1
-
-                idx = Idx(label, (start, stop))
-                subsdict[index] = idx
-
-    # substitute one AntiSymmetricTensor at a time
-    for at in all_AT:
-        inds = Tuple(*(at.upper.args + at.lower.args))
-        inds = inds.subs(subsdict)
-
-        # define arrays with shape because it cannot always be determined from
-        # indices
-        if at.symbol in symbols('t lhs'):
-            shp = (total_orbs,)*(len(inds)%2) + (below_orbs,)*(len(inds)%2)
-        elif at.symbol in symbols('fvwh'):
-            shp = (total_orbs,)*len(inds)
-        else:
-            raise ValueError("Couldn't determine shape of %s" % at)
-
-        base = IndexedBase("%s_%i" %(at.symbol, len(inds)), shape=shp)
-
-        new_at = base[inds.args]
-        expr = expr.subs(at, new_at)
-
-        print "substituted %s for %s" %(new_at, at)
-
-    return expr
 
 
 if __name__ == "__main__":
